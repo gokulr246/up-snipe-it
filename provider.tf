2b7e18a8dbe155a8f7cc214a43699849a1d4c14d
Gokul R <106096073+gokulr246@users.noreply.github.com>
2024-02-12 15:08:20 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,3 +8,12 @@ terraform {
 }
 
 
+provider "azurerm" {
+  subscription_id = "f14b2ec6-1b20-4c56-8ca8-d1f7a23a73cd"
+  tenant_id       = "1856c5f7-0771-4489-a163-b2b346c56e20"
+
+  features {
+
+  }
+}
+
